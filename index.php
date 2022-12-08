21d69360e0e5f69a47888108c676e866477435ef
Hassan Sardar <hassan12590@yahoo.com>
2022-12-08 09:26:47 +0500

    new files added

--- index.php
+++ index.php
@@ -1,19 +1,68 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Php page for Setting</title>
-</head>
-<body>
-<form action="action.php" method="get">
-Name: <input type="text" name="name" /><br /><br />
-Age: <input type="text" name="age" /><br /><br />
-<input type="submit" name="Button" value="Button" />
-</form>
-<h1>Hellow thsi is my first page</h1>
-
     
-</body>
-</html>
+<?php
+    $title = 'Index'; 
+
+    require_once 'includes/header.php'; 
+    require_once 'db/conn.php'; 
+
+    $results = $crud->getSpecialties();
+
+?>
+    <!-- 
+        - First name
+        - Last Name
+        - Date of Birth (Use DatePicker)
+        - Specialty (Database Admin, SOftware Developer, Web Administrator, Other)
+        - Email Address
+        - Contact Number
+     -->
+    <h1 class="text-center">Registration for IT Conference </h1>
+
+    <form method="post" action="success.php" enctype="multipart/form-data">
+        <div class="form-group">
+            <label for="firstname">First Name</label>
+            <input required type="text" class="form-control" id="firstname" name="firstname">
+        </div>
+        <div class="form-group">
+            <label for="lastname">Last Name</label>
+            <input required type="text" class="form-control" id="lastname" name="lastname">
+        </div>
+        <div class="form-group">
+            <label for="dob">Date Of Birth</label>
+            <input type="text" class="form-control" id="dob" name="dob">
+        </div>
+        <div class="form-group">
+            <label for="specialty">Area of Expertise</label>
+            <select class="form-control" id="specialty" name="specialty">
+                <?php while($r = $results->fetch(PDO::FETCH_ASSOC)) {?>
+                   <option value="<?php echo $r['specialty_id'] ?>"><?php echo $r['name']; ?></option>
+                <?php }?>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="email">Email address</label>
+            <input required type="email" class="form-control" id="email"  name="email" aria-describedby="emailHelp" >
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-groupform-group">
+            <label for="phone">Contact Number</label>
+            <input type="text" class="form-control" id="phone" name="phone" aria-describedby="phoneHelp" >
+            <small id="phoneHelp" class="form-text text-muted">We'll never share your number with anyone else.</small>
+        </div>
+        <br/>
+        <div class="custom-file">
+            <input type="file" accept="image/*" class="custom-file-input" id="avatar" name="avatar" >
+            <label class="custom-file-label" for="avatar">Choose File</label>
+            <small id="avatar" class="form-text text-danger">File Upload is Optional</small>
+
+        </div>
+        
+        
+        <button type="submit" name="submit" class="btn btn-primary btn-block">Submit</button>
+    </form>
+<br>
+<br>
+<br>
+<br>
+<br>
+<?php require_once 'includes/footer.php'; ?>
